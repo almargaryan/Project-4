2dbb5ccb47e5e316d065609c53501a043111ecb5
almargaryan <163741067+almargaryan@users.noreply.github.com>
2024-03-20 20:41:56 +0400

    change

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Links from "./Links";
+
+function Header(props) {
+    return (
+        <div className="header">
+            <Links/>
+        </div>
+    );
+}
+
+export default Header;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import Header from "../components/Header";
 import {Link} from "react-router-dom";
 
 function Contact(props) {
